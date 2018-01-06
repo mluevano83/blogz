78938aebc6dabd0f4b640661a182b1111b7815b6
Miguel Luevano <miguel.luevano@gmail.com>
2018-01-06 13:54:55 -0600

    ready for use cases

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogpassworde@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ZAl7jeFRnQTKvAVy@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route("/blog", methods=['POST', 'GET'])
+def blog():
+    blogs = Blog.query.all()
+    return render_template("blog.html", heading="This Is Your Blog", blogs=blogs)
+
+@app.route("/blog")
+def post_page():
+        blog = session.query(blog).get(blog.id)
+        return render_template("post.html") 
+
+@app.route("/newpost", methods=['POST', 'GET'])
+def newpost():
+    title = ""
+    body = ""
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        if not title and not body:
+            flash("Please enter a title for your blog")
+            flash("Please enter a body for your blog")
+            return render_template("newpost.html", heading="New Blog Entry")
+        elif not title:
+            flash("Please enter a title for your blog")
+            return render_template("newpost.html", heading="New Blog Entry", body=body)
+        elif not body:
+            flash("Please enter content for your blog")
+            return render_template("newpost.html", heading="New Blog Entry", title=title)
+        else:
+            new_post = Blog(title, body)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/blog')
+
+    return render_template("newpost.html", heading="New Blog Entry")
+
+if __name__ == '__main__':
+    app.run()
